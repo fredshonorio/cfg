0a63a81772448aa1b07b5ee53710f392f9764307
Fred Honorio <fredshonorio@gmail.com>
2016-03-25 20:47:01 +0000

    Added symlink.

--- lib/fs.py
+++ lib/fs.py
@@ -1,7 +1,17 @@
 from lib.execute import call, do_when, swallow
-from os.path import expanduser, dirname, isfile
+from os.path import expanduser, dirname, isfile, abspath
 
 MERGE_TOOL = "meld"
+
+def readlink(lnk):
+    if not isfile(lnk):
+        return None
+    r = swallow(["readlink", "-f", lnk])
+    return None if r.failed else r.out.decode("utf8").strip()
+
+def is_symlink_to(lnk, real):
+    l = readlink(lnk)
+    return False if l is None else l == real
 
 def files_are_not_equal(src, dest):
     return swallow(["diff", src, dest]).failed
@@ -15,3 +25,12 @@ def merge(src, dest, mkdir=False):
             call(["touch", dest])
         call([MERGE_TOOL, src, dest])
     do_when(cmd, lambda: files_are_not_equal(src, dest))()
+
+def symlink(file, link, sudo=False):
+    file, link = map(lambda p: abspath(expanduser(p)), [file, link])
+    def cmd():
+        l = readlink(file)
+        if l:
+            raise Exception("It's a symlink to somewhere else")
+        call(["ln", "-s", file, link], sudo=sudo)
+    do_when(cmd, lambda: not is_symlink_to(link, file))()
