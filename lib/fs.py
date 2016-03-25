35d5619654a1b3922cfdeda187f1740ba86909a8
Fred Honorio <fredshonorio@gmail.com>
2016-03-25 20:06:49 +0000

    Added mkdir option to fs.merge.

--- lib/fs.py
+++ lib/fs.py
@@ -1,12 +1,17 @@
 from lib.execute import call, do_when, swallow
-from os.path import expanduser
+from os.path import expanduser, dirname, isfile
 
 MERGE_TOOL = "meld"
 
 def files_are_not_equal(src, dest):
     return swallow(["diff", src, dest]).failed
 
-def merge(src, dest):
+def merge(src, dest, mkdir=False):
     src, dest = map(expanduser, [src, dest])
-    do_when(lambda: call([MERGE_TOOL, src, dest]),
-            lambda: files_are_not_equal(src, dest))()
+    def cmd():
+        if mkdir:
+            call(["mkdir", "-p", dirname(dest)])
+        if not isfile(dest):
+            call(["touch", dest])
+        call([MERGE_TOOL, src, dest])
+    do_when(cmd, lambda: files_are_not_equal(src, dest))()
