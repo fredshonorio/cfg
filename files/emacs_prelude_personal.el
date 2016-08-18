33294cb88b911fad39c02a7e105293dd48ca1884
Fred Honorio <fredshonorio@gmail.com>
2016-08-18 12:49:20 +0100

    Fonts, org-mode style, and ducks

--- files/emacs_prelude_personal.el
+++ files/emacs_prelude_personal.el
@@ -6,4 +6,9 @@
 (load-theme 'monokai t)
 
 ;;; fonts
-(add-to-list 'default-frame-alist '(font . "DejaVu Sans Mono-10"))
+(add-to-list 'default-frame-alist '(font . "Droid Sans Mono-11"))
+
+;;; org-mode
+;; don't use diff font in headers
+(custom-set-variables
+ '(org-level-color-stars-only t))
