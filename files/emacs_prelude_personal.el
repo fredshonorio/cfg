268795b11d7b345da0a72be1228a216a6e7645a4
Fred Honorio <fredshonorio@gmail.com>
2017-12-30 16:35:21 +0000

    Emacs font

--- files/emacs_prelude_personal.el
+++ files/emacs_prelude_personal.el
@@ -6,10 +6,10 @@
 (load-theme 'monokai t)
 
 ;;; fonts
-(add-to-list 'default-frame-alist '(font . "Droid Sans Mono-11"))
+(add-to-list 'default-frame-alist '(font . "Iosevka Medium-11"))
 
 ;;; org-mode
-;; don't use diff font in headers
+;; don't use different font in headers
 (custom-set-variables
  '(org-level-color-stars-only t))
 
